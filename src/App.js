9316a46d8249fb8392f68b62b1e54475a33b6373
corgiccori97 <tkdgmlgg7@gmail.com>
2022-06-27 02:38:44 +0900

    d

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>안녕하세요. 포트폴리오입니다.</h1>
+      <header>
+        <nav>
+          <h3>navigation</h3>
+          <button>ABOUT</button>
+          <button>INFO</button>
+          <button>PROJECTS</button>
+          <button>CONTACT</button>
+          <button>COMMENT and LIKE</button>
+        </nav>
+      </header>
+      <h1>안녕하세요. 포트폴리오 소개입니다.</h1>
+      <img src="#" alt="profile" />
+      <p> 안녕하세요.. (소개) </p>
     </div>
-
   );
 }
 
