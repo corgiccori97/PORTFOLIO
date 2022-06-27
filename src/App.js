739903df9d696cce5d0114eac856cc31a00b84fe
corgiccori97 <tkdgmlgg7@gmail.com>
2022-06-27 19:39:26 +0900

    scroll nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './nav.js';
 
@@ -6,7 +5,67 @@ function App() {
   return (
     <div>
       <Nav />
-      <section id="about">ABOUT</section>
+      <section id="about">
+        ABOUT
+        Quis minim ex adipisicing reprehenderit ipsum excepteur occaecat voluptate anim in dolor aute in. Irure nostrud consequat irure nisi. Commodo nisi proident excepteur enim ut magna minim eiusmod magna do aliquip.
+
+        Ad veniam qui nostrud enim eu deserunt qui. Aliqua veniam ad anim excepteur magna nisi qui irure voluptate consectetur voluptate in culpa. Veniam sunt nulla non est esse ullamco. Minim reprehenderit Lorem excepteur magna aliquip non eiusmod.
+
+        Minim adipisicing sit est anim. Sunt dolor ut ea qui non commodo ad eu consequat culpa. Elit mollit enim amet labore duis ea minim enim. Tempor ipsum est ex laboris quis aliquip magna aliquip adipisicing.
+      </section>
+      <hr /> 
+      <section id="skill">
+        SKILL
+        Quis minim ex adipisicing reprehenderit ipsum excepteur occaecat voluptate anim in dolor aute in. Irure nostrud consequat irure nisi. Commodo nisi proident excepteur enim ut magna minim eiusmod magna do aliquip.
+
+        Ad veniam qui nostrud enim eu deserunt qui. Aliqua veniam ad anim excepteur magna nisi qui irure voluptate consectetur voluptate in culpa. Veniam sunt nulla non est esse ullamco. Minim reprehenderit Lorem excepteur magna aliquip non eiusmod.
+
+        Minim adipisicing sit est anim. Sunt dolor ut ea qui non commodo ad eu consequat culpa. Elit mollit enim amet labore duis ea minim enim. Tempor ipsum est ex laboris quis aliquip magna aliquip adipisicing.
+      </section>
+      <hr />
+      <section id="projects">
+        PROJECTS
+        Pariatur labore qui cupidatat aute cupidatat exercitation nulla cupidatat velit sunt aute ut reprehenderit aliqua. Proident aliqua eiusmod do pariatur pariatur sunt ad fugiat consequat anim mollit do. Deserunt eiusmod esse ullamco duis ullamco ea. Nostrud exercitation duis velit elit.
+
+Reprehenderit culpa qui laboris nulla veniam tempor dolor. Incididunt officia irure fugiat tempor mollit non ipsum eu ex. Labore elit eiusmod esse aliqua elit laborum enim nisi qui in qui voluptate ea. Ad qui et aliqua aliqua velit mollit minim commodo voluptate commodo laborum fugiat ea adipisicing. Cillum aliqua laboris laborum aliqua quis laboris veniam nostrud officia aliquip proident. Consectetur ea laborum quis labore elit voluptate cupidatat incididunt proident aliquip consectetur dolor cillum. Consectetur elit fugiat velit proident elit et commodo fugiat enim occaecat reprehenderit est deserunt occaecat.
+
+Nostrud excepteur ea proident voluptate ut laboris sunt minim laborum esse velit consequat magna id. Aliquip cillum dolore ea sit aliquip laborum. Ut dolor velit et tempor veniam laboris.
+
+Commodo velit qui consectetur aute laborum adipisicing deserunt pariatur fugiat labore ea duis mollit. Quis anim quis aliqua ut sit velit cupidatat. Mollit dolor officia et magna eiusmod aliqua nisi proident enim irure pariatur esse adipisicing occaecat. Ad laborum sint commodo irure minim dolor eiusmod do exercitation. Eu tempor velit Lorem velit. Excepteur voluptate excepteur ad nostrud nulla sint enim do mollit pariatur aliquip quis dolor nulla.
+
+Eiusmod sit minim Lorem esse et ad ipsum est duis. Exercitation proident ullamco veniam minim et id duis commodo sit. Excepteur velit aute Lorem nulla reprehenderit ullamco commodo proident. Commodo consequat adipisicing aliqua labore elit sint excepteur enim consequat. Do culpa consequat occaecat cupidatat occaecat nulla voluptate nulla fugiat. Ex eu dolor eu pariatur amet irure ipsum cillum. Magna pariatur velit non in nulla occaecat dolor irure enim fugiat id magna.
+
+Exercitation Lorem nisi irure qui nostrud aute ex duis amet. Aliquip sunt ad qui anim aliqua cupidatat pariatur nostrud fugiat aliquip ad amet. Ipsum in ex ipsum proident consequat id culpa quis aliqua officia commodo cupidatat sint. Elit proident et laborum in ex ut adipisicing aliquip est ipsum ea irure in non. Cillum consectetur est veniam ex enim ipsum est consequat ut excepteur laboris Lorem. Nulla ipsum ullamco minim incididunt tempor deserunt duis aute laboris commodo.
+
+Quis velit sit proident irure officia reprehenderit velit amet in sint labore esse. Irure aliquip veniam Lorem aliqua. Do occaecat officia ullamco officia in nulla nulla excepteur. Laborum ex fugiat commodo nulla ut mollit ipsum ipsum labore commodo in magna consequat.
+        Quis minim ex adipisicing reprehenderit ipsum excepteur occaecat voluptate anim in dolor aute in. Irure nostrud consequat irure nisi. Commodo nisi proident excepteur enim ut magna minim eiusmod magna do aliquip.
+
+        Ad veniam qui nostrud enim eu deserunt qui. Aliqua veniam ad anim excepteur magna nisi qui irure voluptate consectetur voluptate in culpa. Veniam sunt nulla non est esse ullamco. Minim reprehenderit Lorem excepteur magna aliquip non eiusmod.
+
+        Minim adipisicing sit est anim. Sunt dolor ut ea qui non commodo ad eu consequat culpa. Elit mollit enim amet labore duis ea minim enim. Tempor ipsum est ex laboris quis aliquip magna aliquip adipisicing.
+      </section>
+      <hr />
+      <section id="contact">
+        CONTACT
+        Id reprehenderit aliquip duis nisi reprehenderit culpa. Eiusmod occaecat deserunt dolor aute veniam aliqua. Esse nulla exercitation ad nulla aliquip deserunt eiusmod aliquip. Ullamco veniam sunt dolor in occaecat voluptate esse est. Aliquip amet do cupidatat cillum deserunt consectetur elit nostrud velit voluptate ut.
+
+Ipsum quis sint aute cupidatat sint id. Et non non aliqua nulla minim ullamco. Proident excepteur cupidatat pariatur ullamco tempor ullamco minim aute aliqua. Labore est veniam consectetur eu. Commodo aute proident fugiat commodo. Nostrud minim anim aliquip laboris id ad velit ex nisi quis ad.
+
+Ut Lorem cillum nulla ex qui occaecat do aliqua nisi officia ipsum quis. Duis nisi dolore Lorem ad nostrud elit sunt. Consectetur cupidatat consequat occaecat mollit adipisicing amet ipsum veniam.
+
+Ipsum cupidatat consectetur qui excepteur labore sint proident dolor officia magna dolore eu irure. Lorem exercitation amet labore est nisi ipsum nostrud magna esse sit cillum ea. Est sint dolore incididunt elit elit cupidatat ullamco tempor ipsum mollit laborum. Dolore labore aliqua ea veniam Lorem sit qui officia id sunt.
+
+Aliquip mollit nostrud aute reprehenderit mollit occaecat magna. Id sit aute proident pariatur nulla sunt non ipsum esse nulla occaecat officia cillum. Sunt deserunt velit proident exercitation consequat ex eiusmod velit reprehenderit mollit ad et ea. Ullamco laborum sunt mollit culpa. Commodo amet nisi mollit quis proident ut ea excepteur dolor voluptate.
+
+Officia cupidatat duis aliqua reprehenderit pariatur ipsum minim quis sit irure veniam. Dolore nisi id cillum id commodo. Magna nostrud quis fugiat ut voluptate sunt deserunt nulla deserunt consectetur dolor irure. Sint adipisicing commodo adipisicing do officia proident esse Lorem excepteur magna. Ex eu ullamco dolor voluptate aute ex ullamco culpa ut eu laborum do irure proident. Fugiat id qui amet consectetur dolor cillum ullamco exercitation aute aliqua tempor.
+
+Laborum do Lorem culpa sunt occaecat culpa Lorem qui Lorem ea qui exercitation aliquip dolore. Nostrud ipsum dolore Lorem consequat tempor. Labore excepteur veniam velit consequat cillum. Ullamco est proident laboris laboris officia consectetur magna magna id cupidatat aliquip eiusmod non. Quis aliquip occaecat do cupidatat id occaecat et do aliquip magna.
+        Quis minim ex adipisicing reprehenderit ipsum excepteur occaecat voluptate anim in dolor aute in. Irure nostrud consequat irure nisi. Commodo nisi proident excepteur enim ut magna minim eiusmod magna do aliquip.
+
+        Ad veniam qui nostrud enim eu deserunt qui. Aliqua veniam ad anim excepteur magna nisi qui irure voluptate consectetur voluptate in culpa. Veniam sunt nulla non est esse ullamco. Minim reprehenderit Lorem excepteur magna aliquip non eiusmod.
+
+        Minim adipisicing sit est anim. Sunt dolor ut ea qui non commodo ad eu consequat culpa. Elit mollit enim amet labore duis ea minim enim. Tempor ipsum est ex laboris quis aliquip magna aliquip adipisicing.
+      </section>
       <h1>안녕하세요. 포트폴리오 소개입니다.</h1>
       <img src="#" alt="profile" />
       <p> 안녕하세요.. (소개) </p>
--- src/nav.js
+++ src/nav.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import { Link, Element } from 'react-scroll'
-
-class ScrollTest extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-}
+import { HashLink as Link } from 'react-router-hash-link';
 
 function Nav() {
     return (
@@ -15,25 +8,30 @@ function Nav() {
             <nav>
                 <ul>
                     <li>
-                        <Link activeClass="active" smooth spy to="about">
-                            ABOUT
+                        <Link to="/#about" smooth={true}>
+                            About
                         </Link>
                     </li>
-                    <li>PROJECTS</li>
-                    <li>CONTACT</li>
-                    <li>COMMENT and LIKE</li>
+                    <li>
+                        <Link to="/#skill" smooth={true}>
+                            Skill
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/#projects" smooth={true}>
+                            Projects
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/#contact" smooth={true}>
+                            Contact 
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </header>
-
-        <Element name='about' className='element'>
-            <div className='scrollTestSector'>
-                포트폴리오 소개입니다.
-            </div>
-        </Element>
-
         </>
     );
-}
+};
 
 export default Nav;
