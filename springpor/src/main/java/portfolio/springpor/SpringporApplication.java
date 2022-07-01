b9d5a53027ab59b925f136ae47f9164e99cc90f2
RANI <sokoyo2@gmail.com>
2022-07-01 20:38:25 +0900

    springpor

--- springpor/src/main/java/portfolio/springpor/SpringporApplication.java
+++ springpor/src/main/java/portfolio/springpor/SpringporApplication.java
@@ -1,13 +1,13 @@
-package com.example.portfolio;
+package portfolio.springpor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortfolioApplication {
+public class SpringporApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
+		SpringApplication.run(SpringporApplication.class, args);
 	}
 
 }
--- springpor/src/test/java/portfolio/springpor/SpringporApplicationTests.java
+++ springpor/src/test/java/portfolio/springpor/SpringporApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.portfolio;
+package portfolio.springpor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PortfolioApplicationTests {
+class SpringporApplicationTests {
 
 	@Test
 	void contextLoads() {
