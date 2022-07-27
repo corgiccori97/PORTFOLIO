3db4745299decc3c6158deece953bbfefd2a9348
corgiccori97 <tkdgmlgg7@gmail.com>
2022-07-27 23:55:50 +0900

    d

--- src/src/App.js
+++ src/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Nav from './components/Nav.js';
 import About from './components/About.js'
 import Skills from './components/Skills.js'
+import Project from './components/Project';
 import Contact from './components/Contact.js'
 import GlobalStyle from './GlobalStyles.js';
 
@@ -12,29 +13,7 @@ function App() {
       <Nav />
       <About />
       <Skills />
-      <hr />
-      <section id="projects">
-        
-        PROJECTS
-        Pariatur labore qui cupidatat aute cupidatat exercitation nulla cupidatat velit sunt aute ut reprehenderit aliqua. Proident aliqua eiusmod do pariatur pariatur sunt ad fugiat consequat anim mollit do. Deserunt eiusmod esse ullamco duis ullamco ea. Nostrud exercitation duis velit elit.
-
-Reprehenderit culpa qui laboris nulla veniam tempor dolor. Incididunt officia irure fugiat tempor mollit non ipsum eu ex. Labore elit eiusmod esse aliqua elit laborum enim nisi qui in qui voluptate ea. Ad qui et aliqua aliqua velit mollit minim commodo voluptate commodo laborum fugiat ea adipisicing. Cillum aliqua laboris laborum aliqua quis laboris veniam nostrud officia aliquip proident. Consectetur ea laborum quis labore elit voluptate cupidatat incididunt proident aliquip consectetur dolor cillum. Consectetur elit fugiat velit proident elit et commodo fugiat enim occaecat reprehenderit est deserunt occaecat.
-
-Nostrud excepteur ea proident voluptate ut laboris sunt minim laborum esse velit consequat magna id. Aliquip cillum dolore ea sit aliquip laborum. Ut dolor velit et tempor veniam laboris.
-
-Commodo velit qui consectetur aute laborum adipisicing deserunt pariatur fugiat labore ea duis mollit. Quis anim quis aliqua ut sit velit cupidatat. Mollit dolor officia et magna eiusmod aliqua nisi proident enim irure pariatur esse adipisicing occaecat. Ad laborum sint commodo irure minim dolor eiusmod do exercitation. Eu tempor velit Lorem velit. Excepteur voluptate excepteur ad nostrud nulla sint enim do mollit pariatur aliquip quis dolor nulla.
-
-Eiusmod sit minim Lorem esse et ad ipsum est duis. Exercitation proident ullamco veniam minim et id duis commodo sit. Excepteur velit aute Lorem nulla reprehenderit ullamco commodo proident. Commodo consequat adipisicing aliqua labore elit sint excepteur enim consequat. Do culpa consequat occaecat cupidatat occaecat nulla voluptate nulla fugiat. Ex eu dolor eu pariatur amet irure ipsum cillum. Magna pariatur velit non in nulla occaecat dolor irure enim fugiat id magna.
-
-Exercitation Lorem nisi irure qui nostrud aute ex duis amet. Aliquip sunt ad qui anim aliqua cupidatat pariatur nostrud fugiat aliquip ad amet. Ipsum in ex ipsum proident consequat id culpa quis aliqua officia commodo cupidatat sint. Elit proident et laborum in ex ut adipisicing aliquip est ipsum ea irure in non. Cillum consectetur est veniam ex enim ipsum est consequat ut excepteur laboris Lorem. Nulla ipsum ullamco minim incididunt tempor deserunt duis aute laboris commodo.
-
-Quis velit sit proident irure officia reprehenderit velit amet in sint labore esse. Irure aliquip veniam Lorem aliqua. Do occaecat officia ullamco officia in nulla nulla excepteur. Laborum ex fugiat commodo nulla ut mollit ipsum ipsum labore commodo in magna consequat.
-        Quis minim ex adipisicing reprehenderit ipsum excepteur occaecat voluptate anim in dolor aute in. Irure nostrud consequat irure nisi. Commodo nisi proident excepteur enim ut magna minim eiusmod magna do aliquip.
-
-        Ad veniam qui nostrud enim eu deserunt qui. Aliqua veniam ad anim excepteur magna nisi qui irure voluptate consectetur voluptate in culpa. Veniam sunt nulla non est esse ullamco. Minim reprehenderit Lorem excepteur magna aliquip non eiusmod.
-
-        Minim adipisicing sit est anim. Sunt dolor ut ea qui non commodo ad eu consequat culpa. Elit mollit enim amet labore duis ea minim enim. Tempor ipsum est ex laboris quis aliquip magna aliquip adipisicing.
-      </section>
+      <Project />
       <Contact />
     </>
   );
---
+++ src/src/components/Project.js
@@ -0,0 +1,11 @@
+function Project() {
+    return (
+        <section id="projects">
+            <h1>Project</h1>
+
+        
+        </section>
+    );
+}
+
+export default Project;
